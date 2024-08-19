70913d3b6f8d3627078edc0b3cbdd35c3f2ce119
IGOR <i.a.pashko@gmail.com>
2024-08-19 12:56:55 +0300

    task/006 solved-programmatically

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,17 +1,34 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.changeButton)
-        val textView = findViewById<TextView>(R.id.titleTextView)
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.id = R.id.rootLayout
+
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.text = "Hello World!"
+        linearLayout.addView(textView)
+
+        val button = Button(this)
+        button.id = R.id.changeButton
+        button.text = "change"
+        linearLayout.addView(button)
+
+
+        setContentView(linearLayout)
+
         button.setOnClickListener {
             textView.setText(R.string.newText)
 
