35402e98fbb686c04f5fdeb6387de27c550ef500
IGOR <i.a.pashko@gmail.com>
2024-08-30 23:01:03 +0300

    task/012 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -2,33 +2,26 @@ package ru.easycode.zerotoheroandroidtdd
 
 interface Count {
 
-    fun increment(number: String) : String
-    fun isMax(maxString: String) : Boolean
+    fun increment(number: String): UiState
 
     class Base(private val step: Int, private val max: Int) : Count {
+
         init {
             when {
-                step < 1 -> throw IllegalStateException("step shoud be positive, but was $step")
-//                max < 1 -> throw IllegalStateException("max shoud be positive, but was $max")
-//                max <= step -> throw IllegalStateException("max should be more than step")
+                step < 1 -> throw IllegalStateException("step should be positive, but was $step")
+                max < 1 -> throw IllegalStateException("max should be positive, but was $max")
+                max < step -> throw IllegalStateException("max should be more than step")
             }
         }
 
-        override fun increment(number: String): String {
+        override fun increment(number: String): UiState {
             val digits = number.toInt()
             val result = digits + step
-            return result.toString()
-        }
-
-        override fun isMax(maxString: String) : Boolean {
-            val result = maxString.toInt()
-            return if (result == max) {
-                true
+            return if (result + step <= max) {
+                UiState.Base(result.toString())
             } else {
-                false
+                UiState.Max(result.toString())
             }
         }
     }
-
-
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,9 +6,10 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private val count = Count.Base(step = 2, max = 4)
+    private val count = Count.Base(2, 4)
     private lateinit var textView: TextView
     private lateinit var button: Button
+    private var uiState: UiState = UiState.Base("0")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +19,41 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.incrementButton)
 
         button.setOnClickListener {
-            val result = count.increment(textView.text.toString())
-            textView.text = result
-            if (count.isMax(result))
-                button.isEnabled = false
-        }
-
-        if (savedInstanceState != null) {
-            val isButtonDisabled = savedInstanceState.getBoolean(KEY_BUTTON_ENABLED, false)
-            button.isEnabled = !isButtonDisabled
+            uiState = count.increment(textView.text.toString())
+            uiState.apply(textView, button)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean(KEY_BUTTON_ENABLED, !button.isEnabled)
+        outState.putSerializable(KEY, uiState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        uiState = savedInstanceState.getSerializable(KEY) as UiState
+        uiState.apply(textView, button)
     }
 
     companion object {
-        private const val KEY_BUTTON_ENABLED = "button_enabled"
+        private const val KEY = "uiStateKey"
     }
-}
+//            if (count.isMax(result))
+//                button.isEnabled = false
+//        }
+//
+//        if (savedInstanceState != null) {
+//            val isButtonDisabled = savedInstanceState.getBoolean(KEY_BUTTON_ENABLED, false)
+//            button.isEnabled = !isButtonDisabled
+//        }
+//    }
+//
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putBoolean(KEY_BUTTON_ENABLED, !button.isEnabled)
+//    }
+//
+//    companion object {
+//        private const val KEY_BUTTON_ENABLED = "button_enabled"
+//    }
+        }
